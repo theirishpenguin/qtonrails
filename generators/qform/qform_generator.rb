b0aa4911e5bf6de7d40e8127cdd682539b6a08b0
Declan McGrath <declanmg@gmail.com>
2009-12-15 06:43:35 +0000

    Initial commit. Contains qform generator.

---
+++ generators/qform/qform_generator.rb
@@ -0,0 +1,24 @@
+class QformGenerator < Rails::Generator::NamedBase
+
+  def manifest
+    record do |m|
+      qform_dir = 'vendor/plugins/qtonrails/app/qforms'
+      m.directory qform_dir
+      m.template('ui/qform_1col_template.ui', "#{qform_dir}/#{singular_name}_qform.ui", :assigns => {:model_keys => model_keys(eval(class_name).new) } )
+    end
+  end
+
+  def model_keys(a_model)
+
+    # Ensure that timestamps appear at the end if they exist
+    the_model_keys = a_model.attributes.keys.clone
+
+    timestamps = the_model_keys.reject do |k|
+        ['created_at', 'updated_at'].include? k
+    end
+
+    the_model_keys = the_model_keys - timestamps
+    timestamps + the_model_keys
+end
+
+end
---
+++ tasks/qtonrails_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :qtonrails do
+#   # Task goes here
+# end
