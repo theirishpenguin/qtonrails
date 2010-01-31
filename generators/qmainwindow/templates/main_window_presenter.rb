7b6482a5ca849f253264f10e516eba740a2ff9f5
jack <jack@pixie.(none)>
2010-01-31 12:06:19 +0000

    List screen now passes selected record id to edit screen.

--- generators/qmainwindow/templates/main_window_presenter.rb
+++ generators/qmainwindow/templates/main_window_presenter.rb
@@ -6,12 +6,18 @@ require 'app/ui_proxies/qmainwindow.ui.rb'
 # functionality such as connecting slots and signals
 class MainWindow < Qt::MainWindow
 
-    @@active_controller = nil 
+    ID_COLUMN = 0
 
-#    slots 'view_clicked()'
-#    slots 'new_clicked()'
+    @active_controller = nil
+
+    # Expose slots
+    # slots 'view_clicked()'
+    # slots 'new_clicked()'
     slots 'edit_clicked()'
-#    slots 'delete_clicked()'
+    # slots 'delete_clicked()'
+
+    # Example of how to expose a signal
+    # signals 'selectionChanged(QItemSelection &selected)'
 
     # We are then free to put our own code into this class without fear
     # of it being overwritten. Here we add a initialize function which
@@ -20,7 +26,7 @@ class MainWindow < Qt::MainWindow
  
     def initialize(parent, tablemodel, active_controller)
 
-       @@active_controller = active_controller
+       @active_controller = active_controller
 
        # Widgets in Qt can optionally be children of other widgets.
        # That's why we accept parent as a parameter
@@ -41,14 +47,28 @@ class MainWindow < Qt::MainWindow
        # if you want to the full gory details
  
        @ui.setup_ui(self)
+
        @ui.tableView.model = tablemodel
+       @tableview = @ui.tableView
+       @tableview.setColumnHidden(ID_COLUMN, true)
+
+       # TODO: Look into selecting a whole row at a time rather than by cell
+       # Something like @tableview.selectionModel = Qt::ItemSelectionModel::Rows
  
-       #connect(@ui.viewButton, SIGNAL('clicked()'), self, SLOT('view_clicked()'))
-       #connect(@ui.viewButton, SIGNAL('clicked()'), self, SLOT('view_clicked()'))
+       # NNB: No need to store the tablemodel underpinning the view like 
+       # this as it is always available via @tableview.currentIndex().model
+       # 
+       # @tablemodel = tablemodel # ie. Don't do this
+
+       # connect(@ui.viewButton, SIGNAL('clicked()'), self, SLOT('view_clicked()'))
+       # connect(@ui.newButton, SIGNAL('clicked()'), self, SLOT('new_clicked()'))
        connect(@ui.editButton, SIGNAL('clicked()'), self, SLOT('edit_clicked()'))
-       #connect(@ui.viewButton, SIGNAL('clicked()'), self, SLOT('view_clicked()'))
+       # connect(@ui.deleteButton, SIGNAL('clicked()'), self, SLOT('delete_clicked()'))
        connect(@ui.actionQuit, SIGNAL('triggered()'), self, SLOT('close()'))
 
+       # Example of wiring a signal to a slot. Note how the C++ notation is used for the params
+       #connect(@tablemodel, SIGNAL('selectionChanged(QItemSelection &selected)'), self, SLOT('rememberSelection(QItemSelection &selected)'))
+      
     end
 
     def view_clicked
@@ -56,8 +76,29 @@ class MainWindow < Qt::MainWindow
     end
 
     def edit_clicked
-        Router.params[:id] = 1
-        Router.choose({:controller => @@active_controller, :action => 'edit'})
+        
+        Router.params[:id] = record_id()
+        Router.choose({:controller => @active_controller, :action => 'edit'})
     end
 
+    # def rememberSelection(selected)
+    #   Example of a slot implementation
+    # end
+
+     def record_id
+        model_index_to_record_id(@tableview.currentIndex)
+     end
+
+
+     def model_index_to_record_id(index)
+
+       # The child() function allows us to retrieve
+       # the index for any cell in the view
+
+       # The data() call then gives us the data in this
+       # cell. It retuns a QVariant which needs to be
+       # converted using toInt()
+
+       index.child(index.row, ID_COLUMN).data.toInt()
+     end
 end
