8c5b3b26f1b8a0c38a0e9d7babb0dbe05189840b
declanmg <declan@weuseopensource.com>
2010-05-27 08:35:52 +0100

    Added support for generating app with multiple sections.

--- generators/qmainwindow/qmainwindow_generator.rb
+++ generators/qmainwindow/qmainwindow_generator.rb
@@ -2,9 +2,34 @@ class QmainwindowGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
+#require 'ruby-debug' ; debugger
+
+      # Parse any supplied parameters
+      # eg. ["nav_items:Home,ToyMasters,Worlds"] => ["nav_items:Home,ToyMasters,Worlds"]
+      #
+      # Available parameters are
+      # - nav_items: Words representing each different navigation item 
+      #   that will appear in the navigation menu. Navigation items cannot contain spaces,
+      #   instead use CapitalCased nouns which will be split two words. Each navigation item
+      #   should be separated by a comma. In a typical Rails app, each 
+      #   controller would equate to one navigation item. But it's arbitrary, you can include
+      #   a nav item such as Home as your first nav item if you wish to have a home page
+
+      # TODO: Consider allowing a subnav to be generated off 1 level of nested resources
+      # nav_items:Home[Mine;Yours],World[Earth;Mars]
+
+      my_args = {}
+      args.each {|arg| pair = arg.split(':') and my_args[pair[0]] = pair[1]}
+
+      if my_args['nav_items']
+        my_args['nav_items'] = my_args['nav_items'].split(',')
+      else
+        my_args['nav_items'] = []
+      end
+
       qdesigns_dir = 'vendor/plugins/qtonrails/app/qdesigns'
       m.directory qdesigns_dir
-      m.template('ui/qmainwindow_template.ui', "#{qdesigns_dir}/qmainwindow.ui")
+      m.template('ui/qmainwindow_template.ui', "#{qdesigns_dir}/qmainwindow.ui", :assigns => {:nav_items => my_args['nav_items']})
 
       ui_f = qdesigns_dir + '/qmainwindow.ui'
       ui_proxy_filepath = ui_f.gsub('qdesigns', 'ui_proxies') + '.rb'
--- generators/qtify/qtify_generator.rb
+++ generators/qtify/qtify_generator.rb
@@ -1,19 +1,35 @@
-class QtifyGenerator < Rails::Generator::NamedBase
+require 'fileutils'
+
+class QtifyGenerator < Rails::Generator::Base
 
   def manifest
+
     record do |m|
 
-      puts "Beginning Qt-ification of #{class_name} model"
+      # Note: This can currently only generate one controller without hanging as an
+      # existing config/routes.rb under qtonrails will cause trouble for subsequent generations
+
+      args.each do |class_name|
+        puts "Beginning Qt-ification of #{class_name} model"
+        puts '*' * 40
+        puts `script/generate qform #{class_name}`
+        puts `script/generate qpresenters`
+        FileUtils.rm_rf 'vendor/plugins/qtonrails/config/routes.rb'  # HACK! NEEDED AS ANY PROMPT HANGS SCRIPT
+        puts `script/generate qtablemodel #{class_name}`
+        puts `script/generate qcontroller #{class_name}`
+        puts `script/generate qview #{class_name}`
+        puts '*' * 40
+        puts "Completed Qt-ification of #{class_name} model"
+      end
+
+
+      puts "Generating main window"
       puts '*' * 40
-      puts `script/generate qform #{class_name}`
-      puts `script/generate qpresenters`
-      puts `script/generate qtablemodel #{class_name}`
-      puts `script/generate qcontroller #{class_name}`
-      puts `script/generate qview #{class_name}`
-      puts `script/generate qmainwindow`
-      puts `script/generate qmainwindow` # NNB: Must do this last step twice!!! Dunno why
+      FileUtils.rm_rf 'vendor/plugins/qtonrails/app/qdesigns/qmainwindow.ui' # HACK! NEEDED AS ANY PROMPT HANGS SCRIPT
+      puts `script/generate qmainwindow nav_items:#{args.join(',')}`
+      puts `script/generate qmainwindow nav_items:#{args.join(',')}` # NNB: Must do this last step twice!!! Dunno why
       puts '*' * 40
-      puts "Completed Qt-ification of #{class_name} model"
+      puts "Completed main window"
 
     end
   end
